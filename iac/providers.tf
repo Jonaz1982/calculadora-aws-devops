a91725ffce9a41522a89b6154c96f1ee864ec69c
Johnatan González Vélez <ingegonzalez1982@gmail.com>
2025-04-11 18:01:30 -0500

    add main

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,27 +1,8 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    helm = {
-      source = "hashicorp/helm"
-    }
-  }
+  required_version = ">= 1.0"
+  backend "s3" {}
 }
 
 provider "aws" {
-  region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-terraform {
-  backend "s3" {
-    bucket         = var.s3_bucket
-    key            = var.s3_key
-    region         = var.region
-    encrypt        = true
-    dynamodb_table = var.s3_dynamodb_table
-  }
+  region = var.region
 }
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,16 +1,5 @@
-
 variable "region" {
-  default = "us-east-1"
-}
-
-variable "s3_bucket" {
-  description = "Nombre del bucket S3 para el backend remoto"
-}
-
-variable "s3_dynamodb_table" {
-  description = "Nombre de la tabla DynamoDB para el locking"
-}
-
-variable "environment" {
-  default = "dev"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
